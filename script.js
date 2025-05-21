4bdf41f9b41a66482386204428140f7a0fa1341e
isaeidt <isadorasoareseidt12@gmail.com>
2025-05-21 18:05:32 -0300

    criando função excluir tarefa

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
 const form = document.querySelector('form');
 const inputAdicionar = document.querySelector('#input_adicionar')
-const taks = document.querySelector('.task')
+const tasks_list = document.querySelector('.tasks_list')
+const criadas = document.querySelector('#contador_criadas h4')
+const concluidas = document.querySelector('#contador_concluidas h4')
+const textoTarefa = document.querySelector('task-text')
+var contadorCriadas = 0
+var contadorConcluidas = 0
 
 form.onsubmit = function(event){
     event.preventDefault()
-    closeBackground.close()
-    
+    Background.close()
+    criandoTarefa()
 }
 
 function criandoTarefa(){
-    const tarefa = inputAdicionar.value
     
+    contadorCriadas++
+    criadas.innerHTML = contadorCriadas
+
+    const tarefa = inputAdicionar.value
+    const tasks = document.createElement('div');
+    tasks.classList.add('tasks');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-checkbox');
+
+    const taskText = document.createElement('p');
+    taskText.textContent = tarefa;
+    taskText.classList.add('task-text');
+
+    const apagar = document.createElement('button');
+    apagar.type = 'button';
+    apagar.classList.add('apagar');
+
+    const lixo = document.createElement('img')
+    lixo.src = '/img/lixo.png'
+    lixo.classList.add('lixeira')
+
+
+    tasks.appendChild(checkbox);
+    tasks.appendChild(taskText);
+    tasks.appendChild(apagar);
+    apagar.appendChild(lixo);
+    tasks_list.appendChild(tasks);
+
+    inputAdicionar.value = '';
+
+    apagar.addEventListener('click', function (){
+        contadorCriadas--
+        criadas.innerHTML = contadorCriadas
+
+        if(contadorCriadas == 0){
+          Background.open()
+        }
+        tasks.remove();
+    })
     
 }
 
-const closeBackground = {
+const Background = {
     element: document.querySelector('.background'),
 
     close(){
-     closeBackground.element.classList.add('close')
+        Background.element.classList.add('close')
+    },
+    open(){
+        Background.element.classList.remove('close')
     }
-}               
+}  
+
+
+function tarefasConcluidas() {
+    textoTarefa.classList.add('riscado')
+}
+
+
+
+
+
+
+
